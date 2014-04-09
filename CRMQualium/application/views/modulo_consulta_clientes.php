a5f0abbdfbb855d2231d6909883969843bafe0d8
Rodrigo Viana <rodviana92@gmail.com>
2014-04-09 12:48:55 -0500

    tabla clientes

---
+++ CRMQualium/application/views/modulo_consulta_clientes.php
@@ -0,0 +1,163 @@
+      <table id="tbla_cliente" class="table table-striped">      
+            <tr>
+                <th>Todos<input type="checkbox"></th>
+                <th></th>
+                <th>&nbsp;Nombre Comercial<input class="form-control" type="text" placeholder="Buscar"></th>
+                <th>&nbsp;Correo
+                <th>&nbsp;&nbsp;Representante<input class="form-control" type="text" placeholder="Buscar"></th></th>
+                <th>Ultima Actividad</th>
+                <th>Operaciones</th>
+
+            </tr>
+            <tr>
+                <td class="contenido_prospecto"><input  type="checkbox"></td>
+                <td><img src="img/fotosClientes/cliente3.jpg" alt="" class="img-thumbnail"></td>
+                <td>Instituto Mexicano Del Seguro Social</td>
+                <td>Sector Salud</td>
+                <td>Enrique Peña Nieto</td>
+                <td>04/06/2014</td>
+                <td class="icon-operaciones">
+                    <span class="icon-uniF478"  data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
+                    <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
+                    <span class="icon-email"  data-toggle="tooltip" data-placement="top" title="Enviar"></span>
+                    <span class="icon-uniF488"  data-toggle="tooltip" data-placement="top" title="Ver contacto"></span>
+                </td>
+            </tr>
+            <tr>
+                <td><input type="checkbox"></td>
+                <td><img src="img/fotosClientes/cliente4.png" alt="" class="img-thumbnail"></td>
+                <td>Apple </td>
+                <td>Manufacturera</td>
+                <td>Steve Jobs</td>
+                <td>04/06/2014</td>
+                <td class="icon-operaciones">
+                  <span class="icon-uniF478"  data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
+                  <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
+                  <span class="icon-email"  data-toggle="tooltip" data-placement="top" title="Enviar"></span>
+                  <span class="icon-uniF488"  data-toggle="tooltip" data-placement="top" title="Ver contacto"></span>
+                 </td>
+            </tr>
+             <tr>
+                <td><input type="checkbox"></td>
+                <td><img src="img/fotosClientes/fb.png" alt="" class="img-thumbnail"></td>     <td>Facebook</td>
+                <td>Agropecuaria</td>
+                <td>Mark Zukenberg</td>
+                <td>04/06/2014</td>
+                <td class="icon-operaciones">
+                  <span class="icon-uniF478"  data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
+                  <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
+                  <span class="icon-email"  data-toggle="tooltip" data-placement="top" title="Enviar"></span>
+                   <span class="icon-uniF488"  data-toggle="tooltip" data-placement="top" title="Ver contacto"></span>
+                </td>
+            </tr>
+             <tr>
+                <td><input type="checkbox"></td>
+                <td><img src="img/fotosClientes/youtube.jpg" alt="" class="img-thumbnail"></td>
+                <td>youtube</td>
+                <td>Comercial</td>
+                <td>Steven Chan</td>
+                <td>04/06/2014</td>
+                <td class="icon-operaciones">
+                  <span class="icon-uniF478"  data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
+                  <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
+                  <span class="icon-email"  data-toggle="tooltip" data-placement="top" title="Enviar"></span>
+                   <span class="icon-uniF488"  data-toggle="tooltip" data-placement="top" title="Ver contacto"></span>
+                </td>
+            </tr>
+            <tr>
+                <td><input type="checkbox"></td>
+                <td><img src="img/fotosClientes/tw.png" alt="" class="img-thumbnail"></td>
+                <td>Twitter</td>
+                <td>Transporte</td>
+                <td>Evan Williams</td>
+                <td>04/06/2014</td>
+                <td class="icon-operaciones">
+                  <span class="icon-uniF478"  data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
+                  <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
+                  <span class="icon-email"  data-toggle="tooltip" data-placement="top" title="Enviar"></span>
+                   <span class="icon-uniF488"  data-toggle="tooltip" data-placement="top" title="Ver contacto"></span>
+                 </td>
+            </tr>
+            <tr>
+                <td><input type="checkbox"></td>
+                <td><img src="img/fotosClientes/coliseo.jpg" alt="" class="img-thumbnail"></td>
+                <td>Coliseo Yucatán</td>
+                <td>Centro de espectaculos</td>
+                <td>Rolando Zapata Bello</td>
+                <td>04/06/2014</td>
+                <td class="icon-operaciones">
+                    <span class="icon-uniF478"  data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
+                    <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
+                     <span class="icon-email"  data-toggle="tooltip" data-placement="top" title="Enviar"></span>
+                    <span class="icon-uniF488"  data-toggle="tooltip" data-placement="top" title="Ver contacto"></span>
+                </td>
+            </tr>
+            <tr>
+                <td><input type="checkbox"></td>
+                <td><img src="img/fotosClientes/ford.png" alt="" class="img-thumbnail"></td>
+                <td>Ford Motor Company</td>
+                <td>Comercial</td>
+                <td>Henry Ford</td>
+                <td>04/06/2014</td>
+                <td class="icon-operaciones">
+                    <span class="icon-uniF478"  data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
+                    <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
+                    <span class="icon-email"  data-toggle="tooltip" data-placement="top" title="Enviar"></span>
+                    <span class="icon-uniF488"  data-toggle="tooltip" data-placement="top" title="Ver contacto"></span>
+                </td>
+            </tr>
+            <tr>
+                <td><input type="checkbox"></td>
+                <td><img src="img/fotosClientes/google.jpg" alt="" class="img-thumbnail"></td>
+                <td>Google</td>
+                <td>Publicidad</td>
+                <td>Larry Page</td>
+                <td>04/06/2014</td>
+                <td class="icon-operaciones">
+                    <span class="icon-uniF478"  data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
+                    <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
+                    <span class="icon-email"  data-toggle="tooltip" data-placement="top" title="Enviar"></span>
+                    <span class="icon-uniF488"  data-toggle="tooltip" data-placement="top" title="Ver contacto"></span>
+                </td>
+            </tr>
+            <tr>
+                <td><input type="checkbox"></td>
+                <td><img src="img/fotosClientes/mozilla.jpg" alt="" class="img-thumbnail"></td>
+                <td>Mozilla  Firefox</td>
+                <td>Comunicacion</td>
+                <td>Beto Canul May</td>
+                <td>04/06/2014</td>
+                <td class="icon-operaciones">
+                    <span class="icon-uniF478"  data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
+                    <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
+                    <span class="icon-email"  data-toggle="tooltip" data-placement="top" title="Enviar"></span>
+                    <span class="icon-uniF488"  data-toggle="tooltip" data-placement="top" title="Ver contacto"></span>
+                </td>
+            </tr>
+            <tr>
+                <td><input type="checkbox"></td>
+                <td><img src="img/fotosClientes/che.jpg" alt="" class="img-thumbnail"></td>
+                <td>Chevrolet</td>
+                <td>Comercial</td>
+                <td>Geyser Ramirez Sanches</td>
+                <td>04/06/2014</td>
+                <td class="icon-operaciones">
+                    <span class="icon-uniF478"  data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
+                    <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
+                    <span class="icon-email"  data-toggle="tooltip" data-placement="top" title="Enviar"></span>
+                    <span class="icon-uniF488"  data-toggle="tooltip" data-placement="top" title="Ver contacto"></span>
+                </td>
+             </tr>
+        </table>
+        <button type="button" class="btn btn-default">Eliminar varios</button> 
+    </div> 
+<!-- </section> -->
+
+
+<script type="text/javascript">
+  $(document).on('ready', function(){
+      $('.icon-uniF478').on('click',function(){
+            window.confirm('Estas seguro de eliminar al cliente');
+          });
+        });
+  </script>
