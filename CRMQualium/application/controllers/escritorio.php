f10ab39d504b4d812c588bb10b83a2a0d8c32113
Rodrigo Viana <rodviana92@gmail.com>
2014-05-05 12:35:54 -0500

    escritorio

--- CRMQualium/application/controllers/escritorio.php
+++ CRMQualium/application/controllers/escritorio.php
@@ -41,10 +41,30 @@ class Escritorio extends Api {
 
 	public function proyectos(){
 		$this->area_Estatica();
+		$this->load->view('modulo_proyectos');
+
+		if($this->ruta() == 'modulo_proyectos_nuevo')
+		{
+			$this->load->view($this->ruta());
+		}
+		if($this->ruta() == 'modulo_proyectos_consulta')
+		{
+			$this->load->view($this->ruta());
+		}
 	}
 
 	public function contratos(){
 		$this->area_Estatica();
+		$this->load->view('modulo_contratos');
+
+		if($this->ruta() == 'modulo_contratos_nuevo')
+		{
+			$this->load->view($this->ruta());
+		}
+		if($this->ruta() == 'modulo_contratos_historial')
+		{
+			$this->load->view($this->ruta());
+		}
 	}
 
 	public function cotizacion(){
