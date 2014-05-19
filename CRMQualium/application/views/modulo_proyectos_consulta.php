c0a6d59cd1124e955747601bb26d8e17971365cc
Rodrigo Viana <rodviana92@gmail.com>
2014-05-19 15:33:30 -0500

    modulo proyectos
    BETO ANDATE A LAAAA!!!!!!!!

--- CRMQualium/application/views/modulo_proyectos_consulta.php
+++ CRMQualium/application/views/modulo_proyectos_consulta.php
@@ -1,79 +1,126 @@
-<div id="posicion_infotd">
-   <table id="tbla_cliente" class="table table-striped">      
+  <div id="posicion_infotd">
+   <table id="tbla_cliente" class="table table-striped">
+      <thead>
+        <tr id="color_titulos">
+          <th style="text-align:center;">Todos<input type="checkbox"></th>
+          <th></th>
+          <th>Nombre<input type="text" class="form-control" placeholder="Buscar">
+            <span class="icon-search busqueda"></span>
+          </th>  
+          <th>Responsable<input type="text" class="form-control" placeholder="Buscar">
+            <span class="icon-search busqueda"></span>
+          </th>
+          <th>Status</th>
+          <th>Entrega</th>           
+          <th>Operaciones</th>
+        </tr>
+      </thead>      
+      <tbody>
        <tr>
-           <th>&nbsp;&nbsp;&nbsp;Todos<br><input type="checkbox"></th>
-           <th></th>
-           <th>Nombre<input class="form-control" type="text" placeholder="Buscar"></th>           
-           <th>Responsable<input class="form-control" type="text" placeholder="Buscar"></th>
-           <th>Status</th>
-           <th>&nbsp;&nbsp;Entrega</th>           
-           <th>&nbsp;&nbsp;&nbsp;&nbsp;Operaciones</th>
+         <td><input  type="checkbox"></td>
+         <td><img src="img/fotosClientes/cliente3.jpg" alt="" class="img-thumbnail"></td>
+         <td>FoodBook</td>                     
+         <td>Ivan villamil</td>
+         <td ><span  class="badge color_status">45</span></td> 
+         <td >04/06/2014</td>                   
+         <td class="icon-operaciones">
+           <div class="eliminar_cliente">
+             <span class="icon-trash"   data-toggle="tooltip" data-placement="top" title="Eliminar"></span> 
+           </div>
+            <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
+            <span class="icon-eye"  data-toggle="modal" data-target="#myModal" title="Ver proyecto"></span>
+         </td>
        </tr>
        <tr>
-           <td class="contenido_prospecto"><input  type="checkbox"></td>
-           <td><img src="img/fotosClientes/cliente3.jpg" alt="" class="img-thumbnail"></td>
-           <td>FoodBook</td>
-                     
-           <td>Ivan villamil</td>
-           <td><span id="color_status" class="badge">45</span></td> 
-           <td>04/06/2014</td> 
-                  
-           <td class="icon-operaciones">
-               <div class="eliminar_cliente">
-                    <span class="icon-trash"   data-toggle="tooltip" data-placement="top" title="Eliminar"></span> 
-               </div>
-               <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
-               
-               <span class="icon-eye"  data-toggle="modal" data-target="#myModal" title="Ver contacto"></span>
-           </td>
-       </tr>
-       <tr>
-           <td><input type="checkbox"></td>
-           <td><img src="img/fotosClientes/cliente4.png" alt="" class="img-thumbnail"></td>
-           <td>Kiper </td>                    
-           <td>jose alberto canul may</td>
-           <td><span id="color_status1" class="badge">50</span></td>
-           <td>04/06/2014</td>            
-           <td class="icon-operaciones">
-               <div class="eliminar_cliente">
-                 <span class="icon-trash"   data-toggle="tooltip" data-placement="top" title="Eliminar"></span> 
-               </div>
-               <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
-               
-               <span class="icon-eye"  data-toggle="modal" data-target="#myModal" title="Ver contacto"></span>
-           </td>
+         <td><input type="checkbox"></td>
+         <td><img src="img/fotosClientes/cliente4.png" alt="" class="img-thumbnail"></td>
+         <td>Kiper </td>                    
+         <td>jose alberto canul may</td>
+         <td><span  class="badge color_status1">50</span></td>
+         <td>04/06/2014</td>            
+         <td class="icon-operaciones">
+            <div class="eliminar_cliente">
+              <span class="icon-trash"   data-toggle="tooltip" data-placement="top" title="Eliminar"></span> 
+            </div>
+            <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
+            <span class="icon-eye"  data-toggle="modal" data-target="#myModal" title="Ver proyecto"></span>
+         </td>
         </tr>
         <tr>
-           <td><input type="checkbox"></td>
-           <td><img src="img/fotosClientes/cliente4.png" alt="" class="img-thumbnail"></td>
-           <td>Kiper </td>
-           <td>jose alberto canul may</td>
-           <td><span id="color_status2" class="badge">50</span></td>           
-           <td>04/06/2014</td>    
-           <td class="icon-operaciones">
-               <div class="eliminar_cliente">
-                 <span class="icon-trash"   data-toggle="tooltip" data-placement="top" title="Eliminar"></span> 
-               </div>
-               <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
-               <span class="icon-email"  data-toggle="tooltip" data-placement="top" title="Enviar"></span>
-               <span class="icon-eye"  data-toggle="modal" data-target="#myModal" title="Ver contacto"></span>
-           </td>
+         <td><input type="checkbox"></td>
+         <td><img src="img/fotosClientes/cliente4.png" alt="" class="img-thumbnail"></td>
+         <td>Kiper </td>
+         <td>jose alberto canul may</td>
+         <td><span class="badge color_status1">50</span></td>           
+         <td>04/06/2014</td>    
+         <td class="icon-operaciones">
+           <div class="eliminar_cliente">
+             <span class="icon-trash"   data-toggle="tooltip" data-placement="top" title="Eliminar"></span> 
+           </div>
+            <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
+            <span class="icon-eye"  data-toggle="modal" data-target="#myModal" title="Ver proyecto"></span>
+         </td>
         </tr>
-   </table>
-   <button type="button" class="btn btn-default">Eliminar varios</button> 
-</div>
+        <tr>
+         <td><input type="checkbox"></td>
+         <td><img src="img/fotosClientes/cliente4.png" alt="" class="img-thumbnail"></td>
+         <td>Kiper </td>
+         <td>jose alberto canul may</td>
+         <td><span class="badge color_status">50</span></td>           
+         <td>04/06/2014</td>    
+         <td class="icon-operaciones">
+            <div class="eliminar_cliente">
+              <span class="icon-trash"   data-toggle="tooltip" data-placement="top" title="Eliminar"></span> 
+            </div>
+            <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
+            <span class="icon-eye"  data-toggle="modal" data-target="#myModal" title="Ver proyecto"></span>
+         </td>
+        </tr>
+        <tr>
+         <td><input type="checkbox"></td>
+         <td><img src="img/fotosClientes/cliente4.png" alt="" class="img-thumbnail"></td>
+         <td>Kiper </td>
+         <td>jose alberto canul may</td>
+         <td><span  class="badge color_status2">50</span></td>           
+         <td>04/06/2014</td>    
+         <td class="icon-operaciones">
+           <div class="eliminar_cliente">
+             <span class="icon-trash"   data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
+           </div>
+           <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
+           <span class="icon-eye"  data-toggle="modal" data-target="#myModal" title="Ver proyecto"></span>
+         </td>
+        </tr>
+        <tr>
+         <td><input type="checkbox"></td>
+         <td><img src="img/fotosClientes/cliente4.png" alt="" class="img-thumbnail"></td>
+         <td>Kiper </td>
+         <td>jose alberto canul may</td>
+         <td><span  class="badge color_status2">50</span></td>           
+         <td>04/06/2014</td>    
+         <td class="icon-operaciones">
+           <div class="eliminar_cliente">
+             <span class="icon-trash"   data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
+           </div>
+           <span class="icon-edit2"  data-toggle="tooltip" data-placement="top" title="Editar"></span>
+           <span class="icon-eye"  data-toggle="modal" data-target="#myModal" title="Ver proyecto"></span>
+         </td>
+        </tr>
+      </tbody>  
+    </table>
+    <button type="button" class="btn btn-default">Eliminar varios</button>
+    <button type="button" class="btn btn-default">Entregar</button>  
+  </div>
 </div>
 <!-- ---------------------------Modal consulta informacion del cliente---------- -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" 
-   aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
-                <p class="panel-title"><h4>Información</h4></p>
-                <span id="cerrar_consulta" class="glyphicon glyphicon-remove close" data-dismiss="modal" aria-hidden="true"></span>
-            </div>
-            <div id="contenido_cliente" class="panel-body">
-            </div>
-        </div>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="panel panel-primary">
+      <div class="panel-heading">
+        <p class="panel-title"><h4>Información</h4></p>
+        <span id="cerrar_consulta" class="glyphicon glyphicon-remove close" data-dismiss="modal" aria-hidden="true"></span>
+      </div>
+      <div id="contenido_cliente" class="panel-body"></div>
     </div>
+  </div>
 </div>
--- CRMQualium/application/views/modulo_proyectos_nuevo.php
+++ CRMQualium/application/views/modulo_proyectos_nuevo.php
@@ -1,9 +1,123 @@
-	
 	<script src="js/jquery-ui-1.10.4.custom.js"></script>
    	<h3>Nuevo Proyecto</h3>
 	<hr><br>
 	<input type="text" class="form-control" placeholder="Nombre del proyecto">
-	<select id="" class= "form-control" > 
+	<input type="text" class="form-control" placeholder="Buscar Cliente">
+	<span id="buscar" class="icon-search"></span><br>
+	<table id="cabecera_fija2" class="table table-striped " >
+		<thead>
+			<tr>
+				<th></th>
+				<th>Servicios</th>				
+				<th></th>
+			</tr>
+		</thead>
+	</table>	
+	<div id="servicios_proyecto">
+		<table  class="table table-striped" >
+			<tbody>
+				<tr>
+					<td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+					<td><input id="cuadro_busqueda" type="text" class="form-control" placeholder="Otro servicio">
+					</td>					
+					<td class="icon-operaciones">
+					 <span id="icono_mas" class="icon-uniF476"></span>
+					</td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>Tarjeta de presentación frente</td>					
+					<td></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>Medallón</td>					
+					<td></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>Tríptico</td>					
+					<td></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>díptico</td>					
+					<td></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>Catálogo</td>					
+					<td></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>Tarjeta de presentación frente y vuelta</td>					
+					<td></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>Anuncio sencillo</td>					
+					<td></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>Menú de restaurante</td>					
+					<td></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>Diseño de caja</td>					
+					<td></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>Branding Completo</td>					
+					<td></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>Logo Animado</td>					
+					<td></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>Video Branding</td>					
+					<td></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>Campaña</td>					
+					<td></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>Aplicaciones de campaña</td>					
+					<td></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>Página Web sencilla </td>					
+					<td></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>Página Web complicada (Mas de 5 secciones)</td>					
+					<td></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>Página Web con sistema interno</td>					
+					<td></td>
+				</tr>
+				<tr>
+					<td><input type="checkbox"></td>
+					<td>App sencilla</td>					
+					<td></td>
+				</tr>
+			</tbody>
+	   </table><br>
+	</div><br>
+	<!-- <select id="" class= "form-control" > 
     	<option value="" disabled style='display:none;'>Servicio</option> 
 	    <option>Aplicacion movil </option> 
 	    <option>Pagina web </option> 
@@ -14,15 +128,15 @@
 	    <option>Logo</option> 
 	    <option>App sencilla</option> 
 	    <option selected disabled>Servicio</option>
-    </select><br>
+    </select><br> -->
     <h5><b>Especifique la fecha de inicio y entrega del proyecto</b></h5><br>
     <div id="divfech">	    	
 	    <h5><b>Inicio</b></h5>	    
-	    <input class="form-control datepicker " type="text" id="fechaInicio">	    
+	    <input class="form-control datepicker" type="text" id="fechaInicio">	    
 	</div>
 	<div class="divfech1">
 		<h5><b>Entrega</b></h5>
-		<input class="form-control datepicker fecha" type="text" id="fechaEntrega">	
+		<input class="form-control datepicker" type="text" id="fechaEntrega">	
     </div>
 	<div class="divfech1">
 		<h5><b>Duración</b></h5>
@@ -33,66 +147,73 @@
 	    <label for="exampleInputFile">Adjuntar archivo</label>
 	    <input type="file" id="exampleInputFile">
     </div><br>
+    <h3>Asignar Roles</h3>
+	<hr><br>
     <div id="contenedor_select">
 	    <div id="roles"><b>Empleados</b></div>
 	    <div id="select_empleados">
 		    <select multiple class="form-control">
-				<option data-toggle="modal" data-target="#myModal">Geyser</option>
-				<option data-toggle="modal" data-target="#myModal">Rodrigo</option>
-				<option data-toggle="modal" data-target="#myModal">Beto</option>
-				<option data-toggle="modal" data-target="#myModal">Dante</option>
-				<option data-toggle="modal" data-target="#myModal">Xacur</option>
-				<option data-toggle="modal" data-target="#myModal">Villamil</option>
-				<option data-toggle="modal" data-target="#myModal">Gustavo</option>
-				<option data-toggle="modal" data-target="#myModal">Adriana</option>
-				<option data-toggle="modal" data-target="#myModal">Pablo</option>
+				<option data-toggle="modal" data-target="#myModal">Abril Astrid Jimenez Sosa</option>
+				<option data-toggle="modal" data-target="#myModal">Deysi Elizabeth Ortega Piste</option>
+				<option data-toggle="modal" data-target="#myModal">Pablo Perez Hernandez</option>
+				<option data-toggle="modal" data-target="#myModal">David Arturo Ricalde Ortega</option>
+				<option data-toggle="modal" data-target="#myModal">Adriana María Arjona Pinzón</option>
+				<option data-toggle="modal" data-target="#myModal">Dante Cervantes</option>
+				<option data-toggle="modal" data-target="#myModal">Iváb Israel Villamil Covian</option>
+				<option data-toggle="modal" data-target="#myModal">Gerardo Soria</option>
+				<option data-toggle="modal" data-target="#myModal">Enrique Xacur</option>
+				<option data-toggle="modal" data-target="#myModal">Gustavo Monforte</option>
 				<option data-toggle="modal" data-target="#myModal">Rodrigo Buenfil</option>
 			</select>
 	    </div><br>
 	    <textarea class="form-control" rows="4" placeholder="Descripción"></textarea>
 	</div>
-    <table id="tbla_roles" class="table table-striped">      
-		<tr id="color_tr">							                
-			<th>Empleados</th>
-			<th>Roles</th>
-			<th></th>
-		</tr>
-		<tr>
-		    <td>Geyser gallardo ramirez snachez</td>						                
-		    <td>Programador SR</td>							                
-		    <td class="icon-eliminar">
-			    <div class="eliminar_cliente">
-			    	<span class="icon-circledelete"   data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
-			    </div>
-		    </td>
-	    </tr>
-	    <tr>
-	        <td>Jose alberto Canul May</td>				                
-		    <td>Desarrollador de la api rest</td>
-		    <td class="icon-eliminar">
-			    <div class="eliminar_cliente">
-			    	<span class="icon-circledelete"   data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
-			    </div>				                   
-		    </td>
-	    </tr>
-	    <tr>
-	        <td>Enrique xacur</td>				                
-		    <td>Director general</td>
-		    <td class="icon-eliminar">
-			    <div class="eliminar_cliente">
-			    	<span class="icon-circledelete"   data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
-			    </div>				                   
-		    </td>
-	    </tr>
-	    <tr>
-	        <td>Rodrigo Buenfil</td>				                
-		    <td>Productor de vídeo y audio</td>
-		    <td class="icon-eliminar">
-			    <div class="eliminar_cliente">
-			    	<span class="icon-circledelete"   data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
-			    </div>				                   
-		    </td>
-	    </tr>				    
+    <table id="tbla_roles" class="table table-striped table-curved">      
+		<thead>
+			<tr id="color_th">							                
+				<th  >Empleados</th>
+				<th>Roles</th>
+				<th ></th>
+			</tr>
+	    </thead>
+	    <tbody>
+	    	<tr>
+			    <td>Geyser gallardo ramirez snachez</td>						                
+			    <td>Programador SR</td>							                
+			    <td class="icon-eliminar">
+				    <div class="eliminar_cliente">
+				    	<span class="icon-circledelete"   data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
+				    </div>
+			    </td>
+	        </tr>
+	        <tr>
+		        <td>Jose alberto Canul May</td>				                
+			    <td>Desarrollador de la api rest</td>
+			    <td class="icon-eliminar">
+				    <div class="eliminar_cliente">
+				    	<span class="icon-circledelete"   data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
+				    </div>				                   
+			    </td>
+	        </tr>
+	        <tr>
+		        <td>Enrique xacur</td>				                
+			    <td>Director general</td>
+			    <td class="icon-eliminar">
+				    <div class="eliminar_cliente">
+				    	<span class="icon-circledelete"   data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
+				    </div>				                   
+			    </td>
+	        </tr>
+		    <tr>
+		        <td>Rodrigo Buenfil</td>				                
+			    <td>Productor de vídeo y audio</td>
+			    <td class="icon-eliminar">
+				    <div class="eliminar_cliente">
+				    	<span class="icon-circledelete"   data-toggle="tooltip" data-placement="top" title="Eliminar"></span>
+				    </div>				                   
+			    </td>
+		    </tr>
+        </tbody>		
     </table>
     <div class="desborde"></div><br><br>		
     <button type="button" class="btn btn-primary">Aceptar</button>&nbsp;
@@ -110,17 +231,15 @@
 		      </div><br><br>	
 		      <div class="modal-body">
 		      		<input id="otro_rol" type="text" class="form-control" placeholder="otro rol">
+		      		<button id="agregar_btn" type="button" class="btn btn-default">Agregar</button>
 		      		<div class="desborde"></div>
 		      		<label><h4 style="color:#0492e6;">Roles</h4></label>
 			      	<div id="select_rol">				      	   
-			      	   <select multiple class="form-control" size="5">		
-							<option data-toggle="modal" data-target="#myModal">Director General</option>
-							<option data-toggle="modal" data-target="#myModal">Director de T.I</option>
-							<option data-toggle="modal" data-target="#myModal">Director de Arte</option>
-							<option data-toggle="modal" data-target="#myModal">Gerente de Ventas</option>
-							<option data-toggle="modal" data-target="#myModal">Gerente Administrativo</option>
-							<option data-toggle="modal" data-target="#myModal">Jefe Community Manager</option>
-							<option data-toggle="modal" data-target="#myModal">Diseñador</option>
+			      	   <select multiple class="form-control" >
+			      	   		<option data-toggle="modal" data-target="#myModal">Lider del proyecto</option>		
+			      	   	    <option data-toggle="modal" data-target="#myModal">Community Manager</option>
+							<option data-toggle="modal" data-target="#myModal">Diseñador Gráfico</option>
+							<option data-toggle="modal" data-target="#myModal">Diseñador Gráfico Sr</option>
 							<option data-toggle="modal" data-target="#myModal">Programador</option>
 							<option data-toggle="modal" data-target="#myModal">Practicante</option>
 							<option data-toggle="modal" data-target="#myModal">Productor de Vídeo y Audio</option>
@@ -152,7 +271,7 @@
 			   </div> 
 		       <div class="modal-footer" >
 			      	<div id="botones">
-				    	<button type="button" class="btn btn-default">Agregar</button>&nbsp;&nbsp;
+				    	<button type="button" class="btn btn-default">Aceptar</button>&nbsp;&nbsp;
 			          	<button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>				          		
 			        </div>
 		        </div>
