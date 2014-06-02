2b6d165866f2e83a2df1d969a7c7f6fc590b74b6
maykultman <dolarmkm@gmail.com>
2014-06-02 16:57:01 -0500

    proyecto
    pruebas

--- CRMQualium/application/controllers/proyectos.php
+++ CRMQualium/application/controllers/proyectos.php
@@ -23,9 +23,11 @@ class  Proyectos extends Api {
     }
 
     private function insert_proyecto()
-    {   # La función ipost()... Recupera todos los post que viene desde la petición        
-        $query = $this->proy->insertProyecto($this->ipost());
-        ($query) ? $this->response($query, 201) : $this->response($query, 404);                 
+    {   
+        var_dump($_FILES);
+        # La función ipost()... Recupera todos los post que viene desde la petición        
+        // $query = $this->proy->insertProyecto($this->ipost());
+        // ($query) ? $this->response($query, 201) : $this->response($query, 404);                 
     }
 
     private function get_proyecto($id)
